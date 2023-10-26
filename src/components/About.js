e5cb5eeb0061353a56908d9116b00361589c3971
clairemuiru1 <muiruclaire@gmail.com>
2023-10-26 18:38:57 +0300

    "Commit changes"

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { image } from "../data/data";
 
 function About() {
   return (
-  <div id="about">About
+  <div id="about">
     <h2>About Me</h2>
     <p>ClaireMuiru1</p>
     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"/>
